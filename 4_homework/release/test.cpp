51dada3668d8312b4a3b2eaad3ccb105af455969
alekela <alexytchursin@yandex.ru>
2024-03-31 10:31:31 +0300

    auto test for 4

--- 4_homework/release/test.cpp
+++ 4_homework/release/test.cpp
@@ -7,23 +7,37 @@ using namespace std;
 
 int main() {
 	ifstream fin, fin2;
-	fin.open("outtmp.txt");
+	fin.open("test.txt");
 	fin2.open("example.txt");
 	string eq, eq2;
 	int counter = 1;
 	while (getline(fin, eq)) {
-		getline(fin2, eq2);
-		if (eq != eq2) {
-			std::cerr << "Difference in line " << counter << ":\n";
-			std:cerr << "Right line: " << eq2 << endl;
-			std::cerr << "Incorrect line" << eq << endl;
-			return -1;
+		if (getline(fin2, eq2)){
+			if (eq != eq2) {
+				cerr << "Difference in line " << counter << ":\n";
+				cerr << "Right line: " << eq2 << endl;
+				cerr << "Incorrect line" << eq << endl;
+				fin.close();
+				fin2.close();
+				return 0;
+			}
+			counter++;
 		}
-		counter++;
+		else {
+			cerr << "Different number of lines in files!" << endl;
+			fin.close();
+			fin2.close();
+			return 0;
+		}
+	}
+	if (getline(fin2, eq2)){
+		cerr << "Different number of lines in files222222222222!" << endl;
+		fin.close();
+		fin2.close();
+		return 0;
 	}
 	std::cout << "OK" << endl;
 	fin.close();
 	fin2.close();
 	return 0;
-
 }
