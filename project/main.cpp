adbdeefec346e0df62f10ef821db3615e03327cf
Алексей Чурсин <76111160+alekela@users.noreply.github.com>
2024-04-15 19:05:11 +0300

    Update main.cpp

--- project/main.cpp
+++ project/main.cpp
@@ -14,7 +14,7 @@ void ExitProgram(const char *message, int ans);
 
 WINDOW * win;
 WINDOW * status;
-enum { Empty = 0, Wall = 1, Pellet, Pacman };
+enum { Empty = 0, Wall = 1, Pellet=2, Pacman=3};
 
 
 class Player {
@@ -27,6 +27,8 @@ public:
 
     Player() {
 	    score = 0;
+	    pos_x = 13;
+	    pos_y = 22;
     }
 
     int get_x() {
@@ -52,7 +54,11 @@ private:
 public:
     std::vector<std::vector<int>> field;
 
-    Level(const int _height=29, const int _width=28) : width(_width), height(_height) {
+    Level(const int _height, const int _width) : width(_width), height(_height) {
+        set_level("level1.txt");
+    }
+
+    Level() : width(28), height(29) {
         set_level("level1.txt");
     }
 
@@ -90,10 +96,10 @@ public:
         // display level
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
-		int tmp = field[i][j];
+                int tmp = field[i][j];
                 if (tmp == 0) {
                     chr = ' ';
-                    attr = A_NORMAL;
+                    attr = A_INVIS;
                     wattron(win, COLOR_PAIR(Empty));
                 }
                 else if (tmp == 1) {
@@ -106,6 +112,11 @@ public:
                     attr = A_NORMAL;
                     wattron(win, COLOR_PAIR(Pellet));
                 }
+                else if (tmp == 3) {
+                    chr = 'C';
+                    attr = A_NORMAL;
+                    wattron(win, COLOR_PAIR(Pacman));
+                }
                 /*else if (tmp == 3) {
                     chr = '*';
                     attr = A_BOLD;
@@ -117,6 +128,7 @@ public:
                     wattron(win, COLOR_PAIR(GhostWall));
                 }*/
                 mvwaddch(win, i, j, chr | attr);
+
             }
         }
 
@@ -127,7 +139,6 @@ public:
         //OR display vulnerable ghosts
 
         //Display Pacman
-
         wrefresh(win);
     }
 };
@@ -138,12 +149,15 @@ private:
 	Player player;
 	Level level;
 public:
+
+    Game() {}
+
 	void move_all(int dy, int dx) {
 		int ny = player.get_y() + dy;
 		int nx = player.get_x() + dx;
-		if (level.field[ny][nx] != 1) {
+		if (level.field[ny][nx] != Wall) {
 			player.set_pos(ny, nx);
-			if (level.field[player.get_y()][player.get_x()] == 2) {
+			if (level.field[player.get_y()][player.get_x()] == Pellet) {
 				player.score++;
 			}
 			level.set_sym(player.get_y(), player.get_x(), Pacman);
@@ -151,8 +165,10 @@ public:
 		}
 
 		// move ghosts and if they are in the cell of pacman -life
-		
-		level.drawLevel();
+	}
+
+	void draw() {
+        level.drawLevel();
 	}
 
 };
@@ -165,33 +181,54 @@ int main() {
     win_width = 28;
     CreateWindows(win_height, win_width, 1, 1);
     Game game;
-
-    // main cycle of Game 
-    Level level(win_height, win_width);
-    level.drawLevel();
-    wrefresh(win);
-    sleep(5);
+    char ch;
+    int dx, dy;
+    bool flag = true;
+    while (flag) {
+        ch = getch();
+        dx = 0;
+        dy = 0;
+        if (ch == 'Q' || ch == 'q') {
+            break;
+        }
+        if (ch == KEY_UP || ch == 'W' || ch == 'w') {
+            dy = -1;
+        }
+        else if (ch == KEY_DOWN || ch == 'S' || ch == 's') {
+            dy = 1;
+        }
+        else if (ch == KEY_LEFT || ch == 'A' || ch == 'a') {
+            dx = -1;
+        }
+        else if (ch == KEY_RIGHT || ch == 'D' || ch == 'd') {
+            dx = 1;
+        }
+        game.move_all(dy, dx);
+        game.draw();
+        usleep(10000);
+    }
     ExitProgram("Bye-bye", 0);
     return 0;
 }
 
 void InitCurses() {
- initscr();
- curs_set(0);
- keypad(stdscr, TRUE);
- nodelay(stdscr, TRUE);
- nonl();
- cbreak();
- noecho();
- if (!has_colors()) {
+    initscr();
+    curs_set(0);
+    keypad(stdscr, TRUE);
+    nodelay(stdscr, TRUE);
+    nonl();
+    cbreak();
+    noecho();
+    if (!has_colors()) {
         ExitProgram("Terminal does not support colors!", -1);
- }
+    }
     start_color();
 
 
- init_pair(Empty,    COLOR_WHITE,   COLOR_BLACK);
- init_pair(Wall,      COLOR_WHITE,   COLOR_WHITE);
- init_pair(Pellet,    COLOR_WHITE,   COLOR_BLACK);
+    init_pair(Empty,    COLOR_BLACK,   COLOR_BLACK);
+    init_pair(Wall,      COLOR_WHITE,   COLOR_WHITE);
+    init_pair(Pellet,    COLOR_WHITE,   COLOR_BLACK);
+    init_pair(Pacman,    COLOR_YELLOW,  COLOR_BLACK);
  /*init_pair(PowerUp,   COLOR_BLUE,    COLOR_BLACK);
  init_pair(GhostWall, COLOR_WHITE,   COLOR_CYAN);
  init_pair(Ghost1,    COLOR_RED,     COLOR_BLACK);
@@ -199,17 +236,15 @@ void InitCurses() {
  init_pair(Ghost3,    COLOR_MAGENTA, COLOR_BLACK);
  init_pair(Ghost4,    COLOR_YELLOW,  COLOR_BLACK);
  init_pair(BlueGhost, COLOR_BLUE,    COLOR_RED);*/
- init_pair(Pacman,    COLOR_YELLOW,  COLOR_BLACK);
 }
 
 void CreateWindows(int y, int x, int y0, int x0) {
- win = newwin(y, x, y0, x0);
- //status = newwin(3, x - 1, y + y0 + 1, 1);
+    win = newwin(y, x, y0, x0);
+    //status = newwin(3, x - 1, y + y0 + 1, 1);
 }
 
 void ExitProgram(const char *message, int ans) {
- endwin();
- std::cout << message << std::endl;
- exit(ans);
+    endwin();
+    std::cout << message << std::endl;
+    exit(ans);
 }
-
