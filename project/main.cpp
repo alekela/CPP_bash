07c4f973c4537e68890f423582c775d383f713a6
Алексей Чурсин <76111160+alekela@users.noreply.github.com>
2024-04-17 18:37:41 +0300

    Update main.cpp added monsters

--- project/main.cpp
+++ project/main.cpp
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <fstream>
 #include <vector>
+#include <time.h>
 
 
 void InitCurses();                                  //Start up ncurses
@@ -15,12 +16,13 @@ void ExitProgram(const char *message, int ans);
 WINDOW * win;
 WINDOW * status;
 
-enum { Empty = 0, Wall = 1, Pellet=2, Pacman=3};
+enum { Empty = 0, Wall = 1, Pellet=2, Pacman=3, Ghost = 4};
 
 
 class Player {
 private:
     int pos_x, pos_y;
+    char chrbuf = '>';
 
 public:
     int score;
@@ -28,6 +30,7 @@ public:
 
     Player() {
 	    score = 0;
+	    lifes = 3;
     }
 
     int get_x() {
@@ -41,6 +44,18 @@ public:
     void set_pos(int y, int x) {
 	    pos_y = y;
 	    pos_x = x;
+    }
+
+    void draw_player(int player_dir) {
+        char chr;
+        if (player_dir == 0) {chr = chrbuf;}
+        if (player_dir == -2) {chr = '^';}
+        if (player_dir == -1) {chr = '<';}
+        if (player_dir == 1) {chr = '>';}
+        if (player_dir == 2) {chr = 'v';}
+        chrbuf = chr;
+        wattron(win, COLOR_PAIR(Pacman));
+        mvwaddch(win, pos_y, pos_x, chr);
     }
 
 };
@@ -61,7 +76,6 @@ public:
     Level() : width(28), height(29) {
         set_level("level1.txt");
     }
-
 
     int get_height() {
         return height;
@@ -98,6 +112,22 @@ public:
 	    field[y][x] = sym;
     }
 
+    int get_sym(int y, int x) {
+        if (y >= height) {
+            y -= height;
+        }
+        if (x >= width) {
+            x -= width;
+        }
+        if (y < 0) {
+            y += height;
+        }
+        if (x < 0) {
+            x += width;
+        }
+        return field[y][x];
+    }
+
     void drawLevel(int player_dir) {
         char chr;
         int attr;
@@ -106,22 +136,22 @@ public:
         for (int i = 0; i < height; i++) {
             for (int j = 0; j < width; j++) {
                 int tmp = field[i][j];
-                if (tmp == 0) {
+                if (tmp == Empty) {
                     chr = ' ';
                     attr = A_INVIS;
                     wattron(win, COLOR_PAIR(Pellet));
                 }
-                else if (tmp == 1) {
+                else if (tmp == Wall) {
                     chr = ' ';
                     attr = A_NORMAL;
                     wattron(win, COLOR_PAIR(Wall));
                 }
-                else if (tmp == 2) {
+                else if (tmp == Pellet) {
                     chr = '.';
                     attr = A_NORMAL;
                     wattron(win, COLOR_PAIR(Pellet));
                 }
-                else if (tmp == 3) {
+                else if (tmp == Pacman) {
                     if (player_dir == 0) {chr = chrbuf;}
                     if (player_dir == -2) {chr = '^';}
                     if (player_dir == -1) {chr = '<';}
@@ -131,11 +161,11 @@ public:
                     attr = A_NORMAL;
                     wattron(win, COLOR_PAIR(Pacman));
                 }
-                /*else if (tmp == 3) {
-                    chr = '*';
-                    attr = A_BOLD;
-                    wattron(win, COLOR_PAIR(PowerUp));
-                }
+                else if (tmp == Ghost) {
+                    chr = '&';
+                    attr = A_NORMAL;
+                    wattron(win, COLOR_PAIR(Pellet)); // TEMPORARY
+                }/*
                 else if (tmp == 4) {
                     chr = ' ';
                     attr = A_NORMAL;
@@ -146,8 +176,6 @@ public:
             }
         }
 
-        //Display number of lives, score, and level
-
         //Display ghosts
 
         //OR display vulnerable ghosts
@@ -157,27 +185,73 @@ public:
 };
 
 
+class Monster {
+private:
+    int color = COLOR_WHITE;
+    int pos_x, pos_y;
+
+public:
+    int cell_under_ghost = Empty;
+    Monster() {}
+
+    int get_x() {
+        return pos_x;
+    }
+
+    int get_y() {
+        return pos_y;
+    }
+
+    void set_pos(int y, int x) {
+        pos_y = y;
+        pos_x = x;
+    }
+};
+
+
 class Game {
 private:
 	Player player;
 	Level level;
 	int max_score = 0;
+	int enemy_num;
+
+	std::vector<Monster*> ghosts;
+
 public:
 
-    Game(int height, int width) : level(height, width) {
+    Game(int height, int width, int number_of_enemies) : level(height, width) {
         for (int i = 0; i < level.get_height(); i++) {
             for (int j = 0; j < level.get_width(); j++) {
-                if (level.field[i][j] == Pacman) {
+                if (level.get_sym(i, j) == Pacman) {
                     player.set_pos(i, j);
                 }
-                if (level.field[i][j] == Pellet) {
+                if (level.get_sym(i, j) == Pellet) {
                     max_score++;
+                }
+                if (level.get_sym(i, j) == Ghost) {
+                    Monster* tmp;
+                    tmp = new Monster;
+                    (*tmp).set_pos(i, j);
+                    ghosts.push_back(tmp);
                 }
             }
         }
+        enemy_num = ghosts.size();
     }
 
-	void move_all(int dy, int dx) {
+    ~Game() {
+        for (int i = 0; i < enemy_num; i++) {
+            delete ghosts[i];
+        }
+    }
+
+    void move_all(int dy, int dx, int hard_level) {
+        move_player(dy, dx);
+        move_ghosts(hard_level);
+    }
+
+	void move_player(int dy, int dx) {
         int x = player.get_x();
         int y = player.get_y();
 		int ny = y + dy;
@@ -195,30 +269,95 @@ public:
         if (ny >= level.get_height()) {
             ny = 0;
         }
-		if (level.field[ny][nx] != Wall) {
-			if (level.field[ny][nx] == Pellet) {
+		if (level.get_sym(ny, nx) != Wall) {
+			if (level.get_sym(ny, nx) == Pellet) {
 				player.score++;
 			}
             player.set_pos(ny, nx);
 			level.set_sym(y, x, Empty);
             level.set_sym(ny, nx, Pacman);
 		}
+	}
 
-		// move ghosts and if they are in the cell of pacman -life
+	void move_monster(int y, int x, int ny, int nx, Monster* mon) {
+        if (nx < 0) {
+            nx = level.get_width() - 1;
+        }
+        if (ny < 0) {
+            ny = level.get_height() - 1;
+        }
+        if (nx >= level.get_width()) {
+            nx = 0;
+        }
+        if (ny >= level.get_height()) {
+            ny = 0;
+        }
+
+        mon->set_pos(ny, nx);
+        level.set_sym(y, x, mon->cell_under_ghost);
+        mon->cell_under_ghost = level.get_sym(ny, nx);
+        level.set_sym(ny, nx, Ghost);
+	}
+
+	void move_ghosts(int hard_level) {
+        for (int i = 0; i < enemy_num; i++) {
+            int y = ghosts[i]->get_y();
+            int x = ghosts[i]->get_x();
+            // probability of moving right, up, left, down
+            double probs[4] = {1., 1., 1., 1.};
+
+            if (level.get_sym(y, x + 1) == Wall) { probs[0] = 0; }
+            if (level.get_sym(y - 1, x) == Wall) { probs[1] = 0; }
+            if (level.get_sym(y, x - 1) == Wall) { probs[2] = 0; }
+            if (level.get_sym(y + 1, x) == Wall) { probs[3] = 0; }
+
+            if (x < player.get_x() && probs[0] != 0) { probs[0] += hard_level; }
+            if (y > player.get_y() && probs[1] != 0) { probs[1] += hard_level; }
+            if (x > player.get_x() && probs[2] != 0) { probs[2] += hard_level; }
+            if (y < player.get_y() && probs[3] != 0) { probs[3] += hard_level; }
+
+            int counter = 0;
+            for (int j = 0; j < 4; j++) {
+                counter += probs[j];
+            }
+            for (int j = 0; j < 4; j++) {
+                probs[j] /= counter;
+            }
+
+            srand(time(NULL));
+            double random_num = (double) rand() / RAND_MAX;
+
+            int pos;
+            double counter2 = 0;
+            for (int j = 0; j < 4; j++) {
+                counter2 += probs[j];
+                if (counter2 > random_num) {
+                    pos = j;
+                    break;
+                }
+            }
+
+            if (pos == 0) {move_monster(y, x, y, x + 1, ghosts[i]);}
+            if (pos == 1) {move_monster(y, x, y - 1, x, ghosts[i]);}
+            if (pos == 2) {move_monster(y, x, y, x - 1, ghosts[i]);}
+            if (pos == 3) {move_monster(y, x, y + 1, x, ghosts[i]);}
+
+        }
 	}
 
 	void draw(int player_dir) {
         level.drawLevel(player_dir);
+        //player.draw_player(player_dir)
 	}
 
     void display_status() {
         wmove(status, 1, 1);
-        /*wattron(status, COLOR_PAIR(Pacman));
-        for(a = 0; a < Lives; a++)
+        wattron(status, COLOR_PAIR(Pacman));
+        for(int a = 0; a < player.lifes; a++)
             wprintw(status, "C ");
         wprintw(status, "  ");
-        wattroff(status, COLOR_PAIR(Pacman));*/
-        mvwprintw(status, 1, 1, "Score: %d ", player.score);
+        wattroff(status, COLOR_PAIR(Pacman));
+        wprintw(status, "Score: %d ", player.score);
         wrefresh(status);
     }
 
@@ -235,7 +374,7 @@ int main() {
     win_height = 29;
     win_width = 28;
     CreateWindows(win_height, win_width, 1, 1);
-    Game game(win_height, win_width);
+    Game game(win_height, win_width, 0);
     char ch;
     int dx, dy;
     while (true) {
@@ -258,14 +397,14 @@ int main() {
         else if (ch == KEY_RIGHT || ch == 'D' || ch == 'd') {
             dx = 1;
         }
-        game.move_all(dy, dx);
+        game.move_all(dy, dx, 0);
         game.draw(2 * dy + dx);
         game.display_status();
         if (game.check_end()) {
             ExitProgram("You've won!!!", 0);
             break;
         }
-        usleep(10000);
+        usleep(200000);
     }
     ExitProgram("Bye-bye", 0);
     return 0;
