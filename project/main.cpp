2ddfa95bdd5584d11bae3db4c71462818f44107e
alekela <alexytchursin@yandex.ru>
2024-04-21 14:52:40 +0300

    added window size check

--- project/main.cpp
+++ project/main.cpp
@@ -481,11 +481,21 @@ public:
     Menu(int height, int width) {
         menu = newwin(height, width, 1, 1);
     }
+
+    void draw() {
+    	
+    }
 };
 
 
 int main() {
     InitCurses();
+    int yMax, xMax;
+    getmaxyx(stdscr, yMax, xMax);
+    if (yMax < 35 || xMax < 30) {
+    	ExitProgram("Your terminal must be at least 35 rows and 30 coloms to play game", -1);
+    }
+
     int win_width, win_height;
     win_height = 29;
     win_width = 28;
