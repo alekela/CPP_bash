a445e06f1244d21edbc51f9fa736bd47f54ac7a1
Алексей Чурсин <76111160+alekela@users.noreply.github.com>
2024-04-25 14:06:11 +0300

    added shooting mechanics

--- project/main.cpp
+++ project/main.cpp
@@ -35,11 +35,11 @@ public:
         }
 
         int get_x() {
-        return pos_x;
+        	return pos_x;
         }
 
         int get_y() {
-        return pos_y;
+        	return pos_y;
         }
 
         void set_pos(int y, int x) {
@@ -48,16 +48,16 @@ public:
         }
 
         void draw_player(int player_dir, WINDOW *win) {
-        char chr;
-        if (player_dir == 0) {chr = chrbuf;}
-        if (player_dir == -2) {chr = '^';}
-        if (player_dir == -1) {chr = '<';}
-        if (player_dir == 1) {chr = '>';}
-        if (player_dir == 2) {chr = 'v';}
-        chrbuf = chr;
-        wattron(win, COLOR_PAIR(Pacman));
-        mvwaddch(win, pos_y, pos_x, chr);
-        wattroff(win, COLOR_PAIR(Pacman));
+	        char chr;
+	        if (player_dir == 0) {chr = chrbuf;}
+	        if (player_dir == -2) {chr = '^';}
+	        if (player_dir == -1) {chr = '<';}
+	        if (player_dir == 1) {chr = '>';}
+	        if (player_dir == 2) {chr = 'v';}
+	        chrbuf = chr;
+	        wattron(win, COLOR_PAIR(Pacman));
+	        mvwaddch(win, pos_y, pos_x, chr);
+	        wattroff(win, COLOR_PAIR(Pacman));
         }
 
 };
